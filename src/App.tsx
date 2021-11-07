93fc81f2f8f8a1885b615988b0dcd939cd469357
Arthur <arthur.cunha@acct.global>
2021-11-07 15:47:10 -0300

    change App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Login from "./components/Forms/UserForm/Login";
+import UserRegister from "./components/Forms/UserForm/Register";
+import BookList from "./components/BookList";
+import { connect } from "react-redux";
+import { Container, Title, Wrapper } from "./components/index";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IProps {
+  isLoginPage: boolean;
+  isBookListPage: boolean;
 }
 
-export default App;
+const App: React.FC<IProps> = ({ isLoginPage, isBookListPage }) => {
+  return (
+    <Container>
+      <Title>Biblioteca Soft</Title>
+      {isLoginPage && (
+        <Wrapper>
+          <Login />
+          <UserRegister />
+        </Wrapper>
+      )}
+      {!isBookListPage && <BookList />}
+    </Container>
+  );
+};
+
+const mapStateToProps = (state: any) => ({
+  isLoginPage: state.reducer.isLoginPage,
+  isBookListPage: state.reducer.isBookListPage,
+});
+
+export default connect(mapStateToProps)(App);
