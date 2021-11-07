f1db3b4fa07b9dfb7e6c62361036392ccf72c5a7
Arthur <arthur.cunha@acct.global>
2021-11-07 15:48:40 -0300

    create style file

---
+++ src/components/index.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background-color: #ff9000;
+`;
+
+export const Container = styled.div`
+  margin: 0 auto;
+  max-width: 1300px;
+  min-width: 320px;
+  text-align: center;
+  background-color: #192734;
+`;
+
+export const Input = styled.input`
+  background-color: #232129;
+  color: #ffff;
+`;
+
+export const SubTitle = styled.h3`
+  color: #ffff;
+`;
+
+export const Text = styled.p`
+  color: #ffff;
+`;
+
+export const Title = styled.h1`
+  color: #ffff;
+`;
+
+export const Wrapper = styled.div``;
