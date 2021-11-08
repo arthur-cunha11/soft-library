b328ab50f18f85d75a775ff56f2c184daec706d4
Arthur <arthur.cunha@acct.global>
2021-11-07 23:30:24 -0300

    create rent books function

--- src/components/Book/BookList/index.tsx
+++ src/components/Book/BookList/index.tsx
@@ -14,7 +14,17 @@ const BookList: React.FC<IProps> = ({ bookList }) => {
   const isEditingBook = useSelector(
     (state: any) => state.reducer.isEditingBook
   );
-  const [showDetails, setShowDetails] = useState<string>("");
+  const [showDetails, setShowDetails] = useState<Array<string>>([]);
+  const [rentedBooks, setRentedBooks] = useState<Array<string>>([]);
+
+  const rentBook = (id: string) => {
+    if (verifyRentedBooks(id)) {
+      alert("Este livro já está alugado!");
+    } else {
+      setRentedBooks([...rentedBooks, id]);
+      alert("Livro alugado com sucesso!");
+    }
+  };
 
   const deleteBook = async (id: string) => {
     await fetch("http://localhost:3000/books/" + id, {
@@ -24,25 +34,43 @@ const BookList: React.FC<IProps> = ({ bookList }) => {
     alert("Livro excluído com sucesso!");
   };
 
+  const verifyRentedBooks = (id: string) => {
+    return rentedBooks.some((element: any) => {
+      return element === id;
+    });
+  };
+
+  const verifyShowDetails = (id: string) => {
+    return showDetails.some((element: any) => {
+      return element === id;
+    });
+  };
+
   return (
     <List>
       {bookList.map((element: any) => {
         return (
           <ListItem key={element.id}>
+            {verifyRentedBooks(element.id) && (
+              <Text>Este livro está alugado!</Text>
+            )}
             <Text>Nome: {element.name}</Text>
             <Text>Autor: {element.author}</Text>
-            {showDetails === element.id && (
+            {verifyShowDetails(element.id) && (
               <>
                 <Text>Idioma: {element.language}</Text>
                 <Text>Número de páginas: {element.pageNumber}</Text>
               </>
             )}
             {isEditingBook === element.id && <BookEdit id={element.id} />}
+            <Button
+              onClick={() => setShowDetails([...showDetails, element.id])}
+            >
+              Exibir mais detalhes
+            </Button>
+            <Button onClick={() => rentBook(element.id)}>Alugar</Button>
             <Button onClick={() => dispatch(setEditingBook(element.id))}>
               Editar
-            </Button>
-            <Button onClick={() => setShowDetails(element.id)}>
-              Exibir mais detalhes
             </Button>
             <Button onClick={() => deleteBook(element.id)}>Excluir</Button>
           </ListItem>
